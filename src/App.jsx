aa20cc79e393d35bbb6342824acab24dc989cb19
ErrorDoesNotExist <michaelgonzales927@gmail.com>
2023-11-10 11:23:28 +0800

    adding files and adding gitignore

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hi Muu!</h1>
+      <h1>I Love you very much! 😊😘</h1>
+    </>
+  )
+}
+
+export default App
